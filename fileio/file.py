c190008366aed19a82ba63e715b0a823162dab9c
you@example.com <you@example.com>
2017-07-06 20:59:21 +0800

    add file

--- fileio/file.py
+++ fileio/file.py
@@ -4,6 +4,14 @@ Created on 2017年7月5日
 
 @author: lzs
 '''
-f = open('D:\svn.hw\trunk\basecode.txt','r')
-s = f.read()
-print(s)
+#解决错误UnicodeDecodeError：'gbk' codec can't decode byte 0xba in position 146: illegal multibyte sequence
+#open文件指定encoding或者model改为b,以binary方式读取
+try:
+    f = open('D:/svn.hw/trunk/basecode1.txt','r', encoding='utf-8')
+    s = f.read()
+    print(s)
+except FileNotFoundError as e:
+    print('except:',e) 
+finally:
+    if f:
+        f.close()
--- tes.py
+++ tes.py
@@ -4,3 +4,7 @@ Created on 2017年6月29日
 
 @author: Administrator
 '''
+def Test(object):
+    def __init__(self,name):
+        assert isinstance(name, object)
+        self.__name = name
